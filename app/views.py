2bc0307f5237223398f9a3f4977b3be4e90da567
Ankitha124 <ankithad232002@gmail.com>
2023-06-03 10:44:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ankitha(request):
+    return render(request,'ankitha.html')
+
+
+def bhavya(request):
+    return render(request,'bhavya.html')
